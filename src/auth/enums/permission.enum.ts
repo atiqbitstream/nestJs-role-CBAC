61dc51b31652f3c4ea6dc687438bc395f2f36e07
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-23 12:29:52 +0500

    created a 'permission' enum and have used it as a data type in 'permission' entity's column  with 'name'

---
+++ src/auth/enums/permission.enum.ts
@@ -0,0 +1,9 @@
+export enum ClientPermission{
+    CreateUser = 'permission.create.user',
+    ReadUser = 'permission.read.user',
+    Updateuser = 'permission.update.user',
+    DeleteUser = 'permission.delete.user',
+
+    CreateAnnouncement =  'permission.create.announcement',
+    UpdateAnnouncement = 'permission.update.announcement'
+}
