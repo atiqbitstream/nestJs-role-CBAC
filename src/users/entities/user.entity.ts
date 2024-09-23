311f07453bd4e7e8f602ca638be4ebaa908ebe37
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-23 12:05:08 +0500

    created a user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,17 @@
-export class User {
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
+export class User {
+@PrimaryGeneratedColumn()
+ id:number;
+
+ @Column({unique:true})
+ username:string;
+
+ @Column({unique:true})
+ email:string;
+
+ @Column()
+ password:string
     
 }
