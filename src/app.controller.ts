8de999e5e16e073778305502d24bbdcac47832eb
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-23 21:48:23 +0500

    make endpoints for api to test login signup and refresh token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Body, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { get } from 'http';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private authService :AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login (@Request() req)
+  {
+    return this.authService.login(req.user);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req)
+  {
+    return req.user;
+  }
+
+  @Post('refresh')
+  async refreshToken(@Body('refresh_token') refresh_token: string) {
+    const validUser = await this.authService.validateRefreshToken(refresh_token);
+    if (!validUser) {
+      return { statusCode: 401, message: 'Invalid refresh token' };
+    }
+    return {
+      access_token:await this.authService.generateAccessToken(validUser),
+    };
+  }
+
 }
