a5b85f022af32fb8c4b5bbf35a234beca44a5cc3
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-23 12:28:45 +0500

    created a 'permission' entity

---
+++ src/auth/entities/permission.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ClientPermission } from "../enums/permission.enum";
+
+
+@Entity()
+export class Permission
+{
+  @PrimaryGeneratedColumn()
+  id:number;
+
+  @Column({unique:true})
+  name:ClientPermission;
+}
