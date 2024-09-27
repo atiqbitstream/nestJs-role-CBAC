4d4ed97f27fe01a2132a1e1f75a828119642fb4d
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-27 23:15:08 +0500

    created  a roles decorator

---
+++ src/auth/decorators/roles.decorator.ts
@@ -0,0 +1,8 @@
+import { SetMetadata } from "@nestjs/common";
+import { CLientRole } from "../enums/role.enum";
+
+export const ROLES_METADATA_KEY = 'roles_decorator_key';
+
+export const Roles = (...roles:CLientRole[])=>{
+    SetMetadata(ROLES_METADATA_KEY,roles)
+}
