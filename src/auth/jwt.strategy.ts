5d7f396b208be7cd0801d53beb1e6fd74657b6b8
atiqbitstream <atiq.bitstream@gmail.com>
2024-09-28 18:57:28 +0500

    modified 'jwt streategy' validate function to get user with their 'roles' and 'permissions' not just simple user

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private userService:UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +15,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    const user = await this.userService.findOne(payload.username);  // Fetches the user with roles and permissions
+    console.log("Hi! i am extracting payload data from jwt strategy(validate func) : ",payload);
+    
+    return user;
   }
 }     
